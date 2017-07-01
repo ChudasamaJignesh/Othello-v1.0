b0437013e93547cb7a07d16964c14e91aede7f11
Jignesh <Jignesh-81726@users.noreply.github.com>
2017-07-01 21:48:45 +0530

    Add files via upload

---
+++ src/main_components/Color.java
@@ -0,0 +1,12 @@
+package main_components;
+
+/** 
+ * \brief
+ * Disks are either WHITE or BLACK. NONE is for an empty tile.
+ * @author Jignesh Chudasama
+ */
+public enum Color {
+	WHITE, ///< white Disk
+	BLACK, ///< black Disk
+	NONE   ///< Color is NONE for empty tiles on a Board
+}
---
+++ src/strategies/Difficulty.java
@@ -0,0 +1,12 @@
+package strategies;
+
+/** 
+ * \brief
+ * 3 Different Types of Modes: Easy, Medium, Hard
+ * @author Jignesh Chudasama
+ */
+public enum Difficulty {
+	EASY, 	///< computer's AI difficulty will be EASY
+	MEDIUM, ///< computer's AI difficulty will be MEDIUM
+	HARD   	///< computer's AI difficulty will be HARD
+}
